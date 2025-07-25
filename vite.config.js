fdd307f2f32445e6e3c5e62fc92255f896b67400
satyamsingh-1234 <140342807+satyamsingh-1234@users.noreply.github.com>
2025-07-25 15:25:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Neighbourmart/",  // 👈 Add this line (use exact repo name)
   plugins: [react()],
-})
+});
